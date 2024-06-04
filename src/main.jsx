e56d9174dd57544d85966bae474d4ec6c4f978f6
pedrohkunz <pedrokunz094@gmail.com>
2024-06-04 20:22:46 -0300

    add-imports-from-global-css-files-and-create-a-colors-css-file

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import './assets/global/styles.css'
+import './assets/global/fonts.css'
+import './assets/global/colors.css'
+
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
