835452ed87438fd2cca7908a326852d71881217d
pedrohkunz <pedrokunz094@gmail.com>
2024-06-05 21:44:50 -0300

    fixing imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import './assets/global/styles.css'
-import './assets/global/fonts.css'
-import './assets/global/colors.css'
+import './assets/css/styles.css'
+import './assets/css/fonts.css'
+import './assets/css/colors.css'
 
 import './assets/i18n/i18nify.js'
 
